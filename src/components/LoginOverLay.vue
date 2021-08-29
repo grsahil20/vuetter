92a588ce0057c796b99aa27ae5f0e8a0309c5d80
Sahil Grover <gr.sahil20@gmail.com>
2021-08-29 22:53:34 +0200

    show states for login form acc to input

--- src/components/LoginOverLay.vue
+++ src/components/LoginOverLay.vue
@@ -1,9 +1,33 @@
 <template>
   <div>
     <form action="">
-      <input type="text" v-model="email" placeholder="email" />
-      <input type="password" v-model="password" placeholder="password" />
-      <input type="button" value="Log Me In" @click="submitUserLoginForm" />
+      <input
+        type="email"
+        v-model="email"
+        size="50"
+        placeholder="email"
+        @blur="validateFormRequiredFields"
+        required
+      />
+      <br /><br />
+
+      <input
+        type="password"
+        v-model="password"
+        placeholder="password"
+        @blur="validateFormRequiredFields"
+        size="50"
+        required
+      />
+      <br /><br />
+      <input
+        type="button"
+        value="Log Me In"
+        @click="submitUserLoginForm"
+        size="50"
+        :disabled='isDisabled'
+      />
+      <br />
     </form>
   </div>
 </template>
@@ -17,10 +41,21 @@ export default {
     return {
       email: null,
       password: null,
+      isDisabled: true,
     };
   },
   methods: {
+    validateFormRequiredFields: async function () {
+      if (this.email && this.password) {
+        console.log("email and password present");
+        this.isDisabled = false;
+      } else {
+        console.log("Please enter email and password");
+        this.isDisabled = true;
+      }
+    },
     submitUserLoginForm: async function () {
+      console.log("submitUserLoginForm enter email and password");
       await userLogin(this.email, this.password);
     },
   },
@@ -29,6 +64,13 @@ export default {
 
 <style scoped>
 form input {
-  border: 2px solid red;
+  border: 2px solid blue;
+}
+
+form input:button {
+  border: 2px solid green;
+}
+form input:disabled {
+  border: 2px solid yellow;
 }
 </style>
